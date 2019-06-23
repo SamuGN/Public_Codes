2ccc84cdaaabba325a32d8c4c8e3cc1366c0c0f9
SamuGN <45875754+SamuGN@users.noreply.github.com>
2019-06-23 15:14:14 +0100

    Create pictures_routes.js

---
+++ pictures_routes.js
@@ -0,0 +1,14 @@
+const fotografiasController=require('../controllers').fotografias;
+const md_auth=require('../authenticated/authenticated');
+const cm=require('connect-multiparty');
+const md_upload=cm({uploadDir:'./server/uploads/fotografias'});
+
+module.exports=(app)=>{
+    app.post('/api/fotografia',md_auth.auth,fotografiasController.create);
+    app.put('/api/fotografia/:id',md_auth.auth,fotografiasController.update);
+    app.post('/api/upload-fotografia/:id',[md_auth.auth,md_upload],fotografiasController.uploadFotografia);
+    app.get('/api/get-fotografia/:fotografia/:thumb',fotografiasController.getFotografia);
+    app.get('/api/fotografias',fotografiasController.getAll);
+    app.get('/api/fotografias-admin',md_auth.auth,fotografiasController.getAllAdmin);
+    app.get('/api/fotografia/:id',fotografiasController.getById);
+}
