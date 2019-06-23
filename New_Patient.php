8c219a8819f4cb656b07f4c0e8c6654f3c483e05
SamuGN <45875754+SamuGN@users.noreply.github.com>
2019-06-23 12:49:36 +0100

    Create New_Patient.php

---
+++ New_Patient.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js" >
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+<!--[if IE]>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<![endif]-->
+<title>Grupo heridas Axarquia</title>
+<!-- BOOTSTRAP CORE CSS -->
+<link href="assets/css/bootstrap.css" rel="stylesheet" />
+<!-- ION ICONS STYLES -->
+<link href="assets/css/ionicons.css" rel="stylesheet" />
+<!-- FONT AWESOME ICONS STYLES -->
+<link href="assets/css/font-awesome.css" rel="stylesheet" />
+<!-- FANCYBOX POPUP STYLES -->
+<link href="assets/js/source/jquery.fancybox.css" rel="stylesheet" />
+<!-- STYLES FOR VIEWPORT ANIMATION -->
+<link href="assets/css/animations.min.css" rel="stylesheet" />
+<!-- CUSTOM CSS -->
+<link href="assets/css/style-red.css" rel="stylesheet" />
+<!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+
+</head>
+<body data-spy="scroll" data-target="#menu-section" style="background-image:img/2.jpg">
+<!--MENU SECTION START-->
+<div class="navbar navbar-inverse navbar-fixed-top scroll-me" id="menu-section" >
+<div class="container">
+<div class="navbar-header">
+<img src="assets/img/logo.png" width="50%">
+<a class="navbar-brand" href="#">
+
+
+
+</a>
+
+</div>
+
+
+</div>
+</div>
+<!--MENU SECTION END-->
+<!--HOME SECTION START-->
+
+<!--SERVICE SECTION START-->
+<section id="services" >
+<div class="container">
+<div class="row text-center header">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+<h3>Rellene los datos del paciente</h3>
+<hr/>
+</div>
+</div>
+
+
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+<div class="services-wrapper">
+
+
+
+
+<center>
+<div class="book-form agileits-login">
+
+<form action="nuevo_paciente.php" method="post" name="miform" id="miform">
+<div class="span1_of_1">
+									
+									<div class="form-text">
+										
+										<input type="text" name="Name" placeholder="NOMBRE" required="">
+									</div> 
+								</div>
+<div class="span1_of_1">
+									
+									<div class="form-text">
+										
+										<input type="text" name="address" placeholder="DIRECCION" required="">
+									</div> 
+								</div> 
+								
+								
+								<div class="span1_of_1">
+										
+										<div class="form-text">
+										
+										<input type="text" name="age" placeholder="EDAD" required="">
+									</div> 					
+									</div>
+									
+									<br/>
+<input type="button" class="boton" value="Registrar y continuar">
+</form>
+</div>
+</center>
+</div>
+</div>
+</div>
+
+
+</div>
+</section>
+<!--SERVICE SECTION END-->
+<!--PRICING SECTION START-->
+
+
+
+<!--PRIICING SECTION END-->
+<!--WORK SECTION START-->
+
+<!--WORK SECTION END-->
+<!--TEAM SECTION START-->
+
+<!--TEAM SECTION END-->
+<!--GRID SECTION START-->
+
+<!--GRID SECTION END-->
+<!--CONTACT SECTION START-->
+<section id="contact" >
+<div class="container">
+<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+<h3>Contacto</h3>
+<hr />
+
+</div>
+</div>
+
+<div class="row animate-in" data-anim-type="fade-in-up">
+
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="contact-wrapper">
+<h3>Redes sociales</h3>
+<p>
+También puedes contactar con nosotros en:
+</p>
+<div class="social-below">
+<a href="#" class="btn button-custom btn-custom-two" > Facebook</a>
+<a href="#" class="btn button-custom btn-custom-two" > Twitter</a>
+<a href="#" class="btn button-custom btn-custom-two" > Google +</a>
+</div>
+</div>
+
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="contact-wrapper">
+<h3>Contacto</h3>
+<h4><strong>Email : </strong> grupoheridasaxarquia@gmail.com </h4>
+<h4><strong>Tlf : </strong> 619 486 474 </h4>
+
+</div>
+
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="contact-wrapper">
+<h3>Direccion : </h3>
+<h4>Torre del mar </h4>
+<h4>Axarquia / Málaga</h4>
+<div class="footer-div" >
+&copy; 2017 GHA.es | <a href="http://www.designbootstrap.com/" target="_blank" >por Samuel Garcia</a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+
+</div>
+</section>
+<!--CONTACT SECTION END-->
+
+<!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME -->
+<!-- CORE JQUERY -->
+<script src="assets/js/jquery-1.11.1.js"></script>
+<!-- BOOTSTRAP SCRIPTS -->
+<script src="assets/js/bootstrap.js"></script>
+<!-- EASING SCROLL SCRIPTS PLUGIN -->
+<script src="assets/js/vegas/jquery.vegas.min.js"></script>
+<!-- VEGAS SLIDESHOW SCRIPTS -->
+<script src="assets/js/jquery.easing.min.js"></script>
+<!-- FANCYBOX PLUGIN -->
+<script src="assets/js/source/jquery.fancybox.js"></script>
+<!-- ISOTOPE SCRIPTS -->
+<script src="assets/js/jquery.isotope.js"></script>
+<!-- VIEWPORT ANIMATION SCRIPTS   -->
+<script src="assets/js/appear.min.js"></script>
+<script src="assets/js/animations.min.js"></script>
+<!-- CUSTOM SCRIPTS -->
+<script src="assets/js/custom.js"></script>
+
+<link rel="stylesheet" href="assets/css/jquery-ui.css" />
+<script src="assets/js/jquery-ui.js"></script>
+				  <script>
+						  $(function() {
+							$( "#datepicker,#datepicker1,#datepicker2,#datepicker3" ).datepicker();
+						  });
+				  </script>
+
+
+</body>
+
+</html>
